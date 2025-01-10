1784b57c6115264e11c8b3a9109ab7a9b619f89c
Christian Klem <christian.klem@nav.no>
2025-01-10 10:44:58 +0100

    Kan nå kjøre appen i docker - lagt til shadowJar
    Co-authored-by: Bettina D'Avila Barros <bettina.d.avila.barros@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@
 plugins {
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "2.1.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     application
 }
 
 group = "no.nav"
-version = "0.0.1"
 
 application {
     mainClass.set("no.nav.ApplicationKt")
