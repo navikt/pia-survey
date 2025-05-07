95d7113f674d0e5d8cbcc5dfd7c725272f7799ed
nimajimale <nima0512@gmail.com>
2025-05-07 17:01:56 +0200

    Oppdater avhengigheter og legg på constraint for å dekke sårbarhet i Testcontainers

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
-    kotlin("jvm") version "2.1.10"
-    kotlin("plugin.serialization") version "2.1.10"
+    kotlin("jvm") version "2.1.20"
+    kotlin("plugin.serialization") version "2.1.20"
     id("com.gradleup.shadow") version "8.3.5"
 }
 
@@ -12,10 +12,10 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val ktorVersion = "3.1.1"
-val kotlinVersion = "2.1.10"
+val ktorVersion = "3.1.3"
+val kotlinVersion = "2.1.20"
 val kotestVersion = "5.9.1"
-val testcontainersVersion = "1.20.6"
+val testcontainersVersion = "1.21.0"
 
 dependencies {
     // -- ktor
@@ -30,13 +30,13 @@ dependencies {
     implementation("io.ktor:ktor-server-status-pages-jvm:$ktorVersion")
 
     // -- logs
-    implementation("ch.qos.logback:logback-classic:1.5.17")
-    implementation("net.logstash.logback:logstash-logback-encoder:8.0")
+    implementation("ch.qos.logback:logback-classic:1.5.18")
+    implementation("net.logstash.logback:logstash-logback-encoder:8.1")
 
     // -- DB
     implementation("org.postgresql:postgresql:42.7.5")
-    implementation("com.zaxxer:HikariCP:6.2.1")
-    implementation("org.flywaydb:flyway-database-postgresql:11.3.4")
+    implementation("com.zaxxer:HikariCP:6.3.0")
+    implementation("org.flywaydb:flyway-database-postgresql:11.8.1")
     implementation("com.github.seratch:kotliquery:1.9.1")
 
     // -- div
@@ -64,11 +64,17 @@ dependencies {
         }
         implementation("io.netty:netty-codec-http2") {
             version {
-                require("4.1.119.Final")
+                require("4.2.0.Final")
             }
             because(
                 "Versjoner <4.1.117 er sårbare. Inkludert i ktor 3.1.0",
             )
+        }
+        testImplementation("org.apache.commons:commons-compress") {
+            version {
+                require("1.27.1")
+            }
+            because("testcontainers har sårbar versjon")
         }
     }
 }
