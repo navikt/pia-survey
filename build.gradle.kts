28a61b7d68de24ff9a340796b3aa5c7c2a490ac0
Christian Klem <christian.klem@nav.no>
2026-03-05 08:42:54 +0100

    Fiks for sårbarheter i jackson-core
    - fjern constraint som ikke trengs

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,16 +59,15 @@ dependencies {
     testImplementation("no.nav.security:mock-oauth2-server:3.0.1")
 
     constraints {
-        implementation("io.netty:netty-codec-http2") {
-            version {
-                require("4.2.9.Final")
-            }
-            because(
-                "ktor-server-netty har sårbar versjon",
-            )
+        implementation("com.fasterxml.jackson.core:jackson-core") {
+            version { require("2.21.1") }
+            because("versjoner < 2.21.1 har sårbarhet. inkludert i ktor-server-auth:3.4.0")
+        }
+        implementation("tools.jackson.core:jackson-core") {
+            version { require("3.1.0") }
+            because("versjoner < 3.1.0 har sårbarhet. inkludert i logstash-logback-encoder:9.0")
         }
     }
-
 }
 
 tasks {
